f16372adec0f5b77980cd597656e75440f9b1cf9
Vaibhav gite <vaibhavgite60@gmail.com>
2023-02-01 22:37:57 +0530

    contact Commit

---
+++ app/src/main/java/com/example/contact/Dial.java
@@ -0,0 +1,27 @@
+package com.example.contact;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Dial extends Fragment {
+
+
+
+    public Dial() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dial, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/contact/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.contact;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity  //implements BottomNavigationView.OnNavigationItemSelectedListener
+ {
+
+    BottomNavigationView bottomNavigationView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this,  R.id.fragmentContainerView);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+
+    }
+}
